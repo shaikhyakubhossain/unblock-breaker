b3991fd988676c3e59b5429009de4251c120711b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-07 14:52:31 +0530

    Set up empty Unreal engine 5 project

---
+++ Source/unBlockBreaker/unBlockBreaker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "unBlockBreaker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unBlockBreaker, "unBlockBreaker" );
